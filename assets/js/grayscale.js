b03fbe8f95285fb28974ad9cfb9acea143f3efdd
Mark Lowel Montealto <marklowelmontealto.14@gmail.com>
2022-07-04 17:53:41 +0800

    day4

--- assets/js/grayscale.js
+++ assets/js/grayscale.js
@@ -41,3 +41,24 @@
   }
 
 })(); // End of use strict
+var lastScrollTop; // This Varibale will store the top position
+
+navbar = document.getElementById('mainNav'); // Get The NavBar
+
+window.addEventListener('scroll',function(){
+ //on every scroll this funtion will be called
+  
+  var scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+  //This line will get the location on scroll
+  
+  if(scrollTop > lastScrollTop){ //if it will be greater than the previous
+    navbar.style.top='-80px';
+    //set the value to the negetive of height of navbar 
+  }
+  
+  else{
+    navbar.style.top='0';
+  }
+  
+  lastScrollTop = scrollTop; //New Position Stored
+});
